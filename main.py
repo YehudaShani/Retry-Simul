d85db6f6b2abe86b5e80d431cf54060e8150b86c
yehuda shani <yehudashani88@gmail.com>
2026-02-04 17:01:09 +0200

    added higher load of tests, saving to file

--- main.py
+++ main.py
@@ -7,11 +7,15 @@ import wallet_differences
 import wallet_state
 import types_of_wallets
 import random
+import json
 
 def main():
     keyCount = 7
     probabilities = computations.generateKeyFaultProbabilityScenarios(step=0.02)
-    probabilities = random.sample(probabilities, 3000)
+    #probabilities = random.sample(probabilities, 3000)
+
+    #save a list of the wallets that stopped at each bitmask
+    stopped_at_bitmasks = []
 
     for index, probability in enumerate(probabilities):
         print(f"Processing probability {index} of {len(probabilities)}")
@@ -23,6 +27,7 @@ def main():
             wallet_bitmasks, wallet_stopped_at_bitmask, wallet_success_probability = baseline_wallet.fill_up_to_threshold(optimal_threshold+1)
 
         if wallet_stopped_at_bitmask is not None:
+            stopped_at_bitmasks.append([wallet_bitmasks, wallet_stopped_at_bitmask, wallet_success_probability])
             print("Baseline wallet bitmasks are: ", wallet_bitmasks)
             print("Baseline wallet stopped at bitmask: ", wallet_stopped_at_bitmask)
             print("Baseline wallet success probability is: ", wallet_success_probability)
@@ -36,6 +41,10 @@ def main():
             print("Probability is: ", probability)
             print("--------------------------------")
             print("--------------------------------")
+
+        #save the list of stopped at bitmasks to a file
+        with open('stopped_at_bitmasks.json', 'w') as f:
+            json.dump(stopped_at_bitmasks, f)
         
 
 if __name__ == "__main__":
